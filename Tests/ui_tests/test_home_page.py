dbf46da16370edf0271ace138225fb5a4eeed07b
Komron Khisomov <normok_9595@bk.ru>
2023-10-30 15:13:30 +0500

    added fixture

--- Tests/ui_tests/test_home_page.py
+++ Tests/ui_tests/test_home_page.py
@@ -1,41 +1,30 @@
 import time
 
 from Pages.PageObjects.ui_pages.HomePage import HomePage
-from Common.CommonFuncs.assertions import Assertions
 from Common.CommonFuncs.waitings import Waiting
 import allure
+import pytest
 
 
-class TestTable:
+class TestHomePage:
 
-    def test_1(self, browser):
+    @pytest.fixture()
+    def setup(self, browser):
         page = HomePage(browser)
-        assertion = Assertions(browser)
         page.open()
         Waiting(browser).explicit_wait(locator_text=page.PROFILE)
         page.click(locator=page.PROFILE)
         page.click(locator=page.CODE_BUTTON)
         page.click(locator=page.RUS_CODE)
-        page.send_keys('9771858108',locator=page.CODE_INPUT)
+        page.send_keys('9771858108', locator=page.CODE_INPUT)
         page.click(locator=page.LOG_IN_BUTTON)
         browser.implicitly_wait(time_to_wait=10)
         time.sleep(5)
+        yield page
+
+    def test_profile_name(self, browser, setup):
+        page = HomePage(browser)
         page.assert_profile_name()
 
 
-    # def test_2(self, browser, stand):
-    #     page = HomePage(browser, stand)
-    #     assertion = Assertions(browser, stand)
-    #     page.open(page='action_log')
-    #     page.move_to_element(locator=page.FIRST_ROW)
-    #     assertion.assert_attribute_has_css_property('background-color', 'rgba(244, 248, 255, 1)',
-    #                                                 locator=page.FIRST_ROW)
-    #
-    # def test_3(self, browser, stand):
-    #     page = HomePage(browser, stand)
-    #     page.open(page='action_log')
-    #     Waiting(browser).wait_until_visibility_of_element(locator_text=page.FIRST_ROW)
-    #     page.web_scroll(direction='down')
-    #     page.assert_table_titles()
-    #     page.assert_filter_btns_hidden()
 
